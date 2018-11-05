82b2e1803d264f041ab558afb96754d803ad6487
Beaudin Marylène <0826062@decinfo.cvm>
2018-11-05 11:03:33 -0500

    clean

--- Saas/gestpro/BaseDonnees.py
+++ Saas/gestpro/BaseDonnees.py
@@ -5,8 +5,6 @@ class  BaseDonnees():
         self.connecteur = sqlite3.connect('SAAS.db')
         self.curseur = self.connecteur.cursor()
         self.creerTables(self.genererListeTables(),self.genererListeConst())
-        self.insertion("stocks", ['date','trans', 'symbol',53.2,5.2])
-        self.selection('Select * FROM stocks ')
         self.connecteur.close()
     
     def genererListeTables(self):
@@ -14,56 +12,56 @@ class  BaseDonnees():
             ['Serveurs', ['id','integer','PRIMARY KEY'], ['IP','integer',''], ['nom','text','UNIQUE']],
             ['Utilisateur', ['id','integer','PRIMARY KEY'], ['nomUtilisateur','text','UNIQUE'], ['motDePasse','text',''], ['chemin_acces_csv','text','']],
             ['Projet', ['id','integer','PRIMARY KEY'], ['nom','text','UNIQUE']],
-            ['Liaison_Util_Projet', ['id_util','integer','NOT NULL'], ['id_projet','integer','NOT NULL'], ['role','text',''],['PRIMARY KEY (id_util,id_projet)','','']],
-            ['AnalyseTextuelle', ['id','integer','PRIMARY KEY'], ['id_projet','integer','NOT NULL'], ['ligne','integer',''],['colonne','integer',''],['id_type','integer','']],
+            ['Liaison_Util_Projet', ['role','text','']],
+            ['AnalyseTextuelle', ['id','integer','PRIMARY KEY'], ['ligne','integer',''],['colonne','integer','']],
             ['TypeMot', ['id','integer','PRIMARY KEY'], ['nom','text','']],
-            ['LigneChat', ['id','integer','PRIMARY KEY'], ['date','date',''],['texte','text',''],['id_filDiscussion','integer',''],['id_utilisateur','integer','']],
-            ['CasUsage', ['id','integer','PRIMARY KEY'], ['id_projet','integer',''],['ligne','text',''],['texte','text','']],
-            ['Scenarii', ['id','integer','PRIMARY KEY'], ['id_casUsage','integer',''],['ligne','text',''],['id_donnees','integer',''],['id_colonne','integer','']],
-            ['FonctionsCRC', ['id','integer','PRIMARY KEY'], ['id_classe','integer',''],['fonction','text','']],
-            ['CollaboCRC', ['id','integer','PRIMARY KEY'], ['idClasse1','integer',''],['idClasse2','text','']],
-            ['FilDeDiscussion', ['id','integer','PRIMARY KEY'], ['id_Projet','integer','']],
+            ['LigneChat', ['id','integer','PRIMARY KEY'], ['date','date',''],['texte','text','']],
+            ['CasUsage', ['id','integer','PRIMARY KEY'],['ligne','text',''],['texte','text','']],
+            ['Scenarii', ['id','integer','PRIMARY KEY'],['ligne','text','']],
+            ['FonctionsCRC', ['id','integer','PRIMARY KEY'],['fonction','text','']],
+            ['CollaboCRC', ['id','integer','PRIMARY KEY']],
+            ['FilDeDiscussion', ['id','integer','PRIMARY KEY']],
             ['TypeForme', ['id','integer','PRIMARY KEY'], ['nom','text','']],
-            ['Objet_Maquette', ['id','integer','PRIMARY KEY'], ['id_position','integer',''], ['id_type','integer',''], ['hauteur','real',''], ['largeur','real',''], ['fill_couleur','real','NULL']],
+            ['Objet_Maquette', ['id','integer','PRIMARY KEY'], ['hauteur','real',''], ['largeur','real',''], ['fill_couleur','real','NULL']],
             ['ColonnesScenarii', ['id','integer','PRIMARY KEY'], ['nom','text',''], ['numero_position','integer','']],
-            ['Cartes', ['id','integer','PRIMARY KEY'], ['id_projet','integer',''], ['id_responsable','integer','DEFAULT NULL'], ['classe','text',''], ['id_carte_heritage','integer',''], ['ordre','integer','']],
-            ['AttributsCRC', ['id','integer','PRIMARY KEY'], ['id_classe','integer',''], ['nomAttributs','text','']],
-            ['Sprint', ['id','integer','PRIMARY KEY'], ['id_projet','integer',''], ['ordre','integer',''], ['date','date','']],
-            ['Tache_Sprint', ['id','integer','PRIMARY KEY'], ['id_sprint','integer',''], ['description','text',''], ['nom','text',''], ['duree','integer','']],
-            ['Taches_Terlow', ['id','integer','PRIMARY KEY'], ['id_projet','integer',''], ['id_colonne_terlow','integer','NOT NULL'], ['ordre','integer',''], ['texte','text','DEFAULT NULL']],
+            ['Cartes', ['id','integer','PRIMARY KEY'], ['classe','text',''], ['ordre','integer','']],
+            ['AttributsCRC', ['id','integer','PRIMARY KEY'], ['nomAttributs','text','']],
+            ['Sprint', ['id','integer','PRIMARY KEY'], ['ordre','integer',''], ['date','date','']],
+            ['Tache_Sprint', ['id','integer','PRIMARY KEY'], ['description','text',''], ['nom','text',''], ['duree','integer','']],
+            ['Taches_Terlow', ['id','integer','PRIMARY KEY'], ['ordre','integer',''], ['texte','text','DEFAULT NULL']],
             ['Colonnes_Terlow', ['id','integer','PRIMARY KEY'], ['type','text','']],
             ['TypeDonneeScenario', ['id','integer','PRIMARY KEY'], ['texte','text','']],
-            ['Objet_Texte', ['id','integer','PRIMARY KEY'], ['id_position','integer',''],['texte','text','']],
+            ['Objet_Texte', ['id','integer','PRIMARY KEY'], ['texte','text','']],
             ['Position',['id','integer','PRIMARY KEY'],['x','real','NOT NULL'],['y','real','NOT NULL']]
             ]
         return listeTables
     
     def genererListeConst(self):
         listeConst = [
-            ['LigneChat', 'fk_lignechat_idUtil', 'id_utilisateur', 'Utilisateur', 'id'],
-            ['LigneChat', 'fk_lignechat_idFilDisc', 'id_filDiscussion', 'filDeDiscussion', 'id'],
-            ['AnalyseTextuelle', 'fk_analyseTextuelle_type', 'id_type', 'TypeMot', 'id'],
-            ['AnalyseTextuelle', 'fk_analyseTextuelle_projet', 'id_projet', 'Projet', 'id'],
-            ['Liaison_Util_Projet', 'fk_liaison_utilisateur', 'id_util', 'Utilisateur', 'id'],
-            ['Liaison_Util_Projet', 'fk_liaison_projet', 'id_projet', 'Projet', 'id'],
-            ['FilDeDiscussion', 'fk_filDiscussion_projet', 'id_projet', 'Projet', 'id'],
-            ['CollaboCRC', 'fk_collaboCrc_classe1', 'idClasse1', 'Cartes', 'id'],
-            ['CollaboCRC', 'fk_collaboCrc_classe2', 'idClasse2', 'Cartes', 'id'],
-            ['FonctionsCRC', 'fk_fonctionsCrc_classe', 'id_classe', 'Cartes', 'id'],
-            ['Scenarii', 'fk_scenarii_casUsage', 'id_casUsage', 'CasUsage', 'id'],
-            ['Scenarii', 'fk_scenarii_donnes', 'id_donnees', 'TypeDonneeScenario', 'id'],
-            ['Scenarii', 'fk_scenarii_colonne', 'id_colonne', 'ColonnesScenarii', 'id'],
-            ['AttributsCRC', 'fk_attributsCrc_classe', 'id_classe', 'Cartes', 'id'],
-            ['Cartes', 'fk_cartes_heritage', 'id_carte_heritage', 'Cartes', 'id'],
-            ['Cartes', 'fk_cartes_responsable', 'idResponsable', 'Utilisateur', 'id'],
-            ['Cartes', 'fk_cartes_projet', 'id_projet', 'Projet', 'id'],
-            ['Objet_Maquette', 'fk_objetMaquette_position', 'id_position', 'Position', 'id'],
-            ['Objet_Maquette', 'fk_objetMaquette_type', 'id_type', 'TypeForme', 'id'],
-            ['TachesTerlow', 'fk_tachesTerlow_projet', 'id_projet', 'Projet', 'id'],
-            ['TachesTerlow', 'fk_tachesTerlow_colonnes', 'id_colonne_terlow', 'Colonnes_Terlow', 'id'],
-            ['Objet_Texte', 'fk_objetTexte_pos', 'id_position', 'Position', 'id'],
-            ['Tache_Sprint', 'fk_tachesSprint_sprint', 'id_sprint', 'Sprint', 'id'],
-            ['Sprint', 'fk_sprint_projet', 'id_projet', 'Projet', 'id'],
+            ['LigneChat', 'id_utilisateur','INTEGER', 'Utilisateur', 'id'],
+            ['LigneChat', 'id_filDiscussion','INTEGER', 'filDeDiscussion', 'id'],
+            ['AnalyseTextuelle', 'id_type','INTEGER', 'TypeMot', 'id'],
+            ['AnalyseTextuelle', 'id_projet','INTEGER', 'Projet', 'id'],
+            ['Liaison_Util_Projet', 'id_util','INTEGER', 'Utilisateur', 'id'],
+            ['Liaison_Util_Projet', 'id_projet','INTEGER', 'Projet', 'id'],
+            ['FilDeDiscussion', 'id_projet','INTEGER', 'Projet', 'id'],
+            ['CollaboCRC', 'idClasse1','INTEGER', 'Cartes', 'id'],
+            ['CollaboCRC', 'idClasse2','INTEGER', 'Cartes', 'id'],
+            ['FonctionsCRC', 'id_classe','INTEGER', 'Cartes', 'id'],
+            ['Scenarii', 'id_casUsage','INTEGER', 'CasUsage', 'id'],
+            ['Scenarii', 'id_donnees','INTEGER', 'TypeDonneeScenario', 'id'],
+            ['Scenarii', 'id_colonne','INTEGER', 'ColonnesScenarii', 'id'],
+            ['AttributsCRC', 'id_classe','INTEGER', 'Cartes', 'id'],
+            ['Cartes', 'id_carte_heritage','INTEGER', 'Cartes', 'id'],
+            ['Cartes', 'idResponsable','INTEGER', 'Utilisateur', 'id'],
+            ['Cartes', 'id_projet','INTEGER', 'Projet', 'id'],
+            ['Objet_Maquette', 'id_position','INTEGER', 'Position', 'id'],
+            ['Objet_Maquette', 'id_type','INTEGER', 'TypeForme', 'id'],
+            ['Taches_Terlow', 'id_projet','INTEGER', 'Projet', 'id'],
+            ['Taches_Terlow', 'id_colonne_terlow','INTEGER', 'Colonnes_Terlow', 'id'],
+            ['Objet_Texte', 'id_position','INTEGER', 'Position', 'id'],
+            ['Tache_Sprint','id_sprint','INTEGER', 'Sprint', 'id'],
+            ['Sprint', 'id_projet', 'INTEGER',  'Projet', 'id'],
             ]
         return listeConst
         
@@ -109,7 +107,7 @@ class  BaseDonnees():
             
     def alterTable(self,listeConst):
         for contrainte in listeConst:
-            stringAlterTable = "ALTER TABLE " + contrainte[0] + " ADD CONSTRAINT " + contrainte[1] + " FOREIGN KEY(" + contrainte[2] + ") REFERENCES " + contrainte[3] + "(" + contrainte[4] + ");"
+            stringAlterTable = "ALTER TABLE " + contrainte[0] + " ADD COLUMN " + contrainte[1] + " " + contrainte[2] + " REFERENCES " + contrainte[3] + "(" + contrainte[4] + ");"
             self.curseur.execute(stringAlterTable)
            
         

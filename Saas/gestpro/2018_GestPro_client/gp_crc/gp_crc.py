ddb03257ee3ecfcbed99593c67cc30e5819cfb51
Beaudin Marylène <0826062@decinfo.cvm>
2018-11-12 10:17:15 -0500

    dfjhdjh

--- Saas/gestpro/2018_GestPro_client/gp_crc/gp_crc.py
+++ Saas/gestpro/2018_GestPro_client/gp_crc/gp_crc.py
@@ -26,36 +26,34 @@ class Controleur():
 
 class Modele():
     def __init__(self):
-        self.bd = BaseDonnees
+        #self.bd = BaseDonnees
         self.nombreCartes=0
         for i in selectClassesCartes():
             self.nombreCartes+=1        
     
     def insertCarte(self,listeValeur):
         bd.insertionPerso("""INSERT INTO Cartes(id_projet,classe, id_carte_heritage, ordre) VALUES(  """ + listeValeur[0] + """, """ + listeValeur[1] + """, """ + listeValeur[2] + """, """ + listeValeur[3] + """, """ + listeValeur[4] + """ )""")
-
     
     def selectClassesCartes(self):
         commande ="""SELECT classe FROM Cartes"""
         #Retourne une liste de String de cartes
-        return self.bd.selection(commande)
+        return bd.selection(commande)
     
     def selectIdCarte(self,classe):
         commande ="""SELECT id FROM Cartes WHERE classe="""
         commande+=classe
-        ID = self.bd.selection(commande)
+        ID = bd.selection(commande)
         return ID
     
     def selectAttributDeCarte(self,idCarte):
         commande = """SELECT nomAttributs FROM AttributsCRC WHERE id_classe="""
         commande+=idCarte
         #Retourne une liste de String des attributs d'UNE carte
-        return self.bd.selection(commande)
+        return bd.selection(commande)
     
     def insertAttributsDeCarte(self,id_classe):
         listeValeur
-        self.bd.insertion("AttributsCRC")
-
+        bd.insertion("AttributsCRC")
     
 if __name__ == '__main__':
     c=Controleur()

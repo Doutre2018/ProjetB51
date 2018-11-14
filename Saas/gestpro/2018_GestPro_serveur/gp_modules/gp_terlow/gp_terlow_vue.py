02fec1c69f6880d33bdc38e293ca79960c146c5e
Doutre-Guay Ludovic <ludovicdguay@gmail.com>
2018-11-14 10:31:37 -0500

    joke

--- Saas/gestpro/2018_GestPro_serveur/gp_modules/gp_terlow/gp_terlow_vue.py
+++ Saas/gestpro/2018_GestPro_serveur/gp_modules/gp_terlow/gp_terlow_vue.py
@@ -9,38 +9,26 @@ from helper import Helper as hlp
 from msilib.schema import Font
 
 class Vue():
-    def __init__(self,parent,largeur=800,hauteur=600):
-        #Variable de BD
-        self.projetName="Projet de Gestion de Projet"
-        self.utilisateursEtRole ={"Joé":"Donnée",
-                                  "Claudia":"Maquette",
-                                  "Ludovic":"Maquette",
-                                  "JF":"Rien",
-                                  "Simon":"Rien",
-                                  "Danick":"Maquette",
-                                  "Marylene":"Rien",}
-        self.dicodesprint={1:"29 oct. 2018",
-                           2:"29 oct. 2018",
-                           3:"29 oct. 2018"}
-        
-        
+    def __init__(self,parent,largeur=1200,hauteur=800):
+
         self.root=tix.Tk()
         self.root.title(os.path.basename(sys.argv[0]))
-        self.root.attributes("-fullscreen", False)
         self.root.protocol("WM_DELETE_WINDOW", self.fermerfenetre)
         self.parent=parent
         self.modele=None
-        self.largeurDefault=largeur/7
-        self.hauteurDefault=hauteur/4.5
-        self.largeur=self.root.winfo_screenwidth()/7
-        self.hauteur=self.root.winfo_screenmmheight()/4.5
-        self.cadrebaseExiste=False
+        
+        self.largeur=self.largeurDefault=largeur
+        self.hauteur=self.hauteurDefault=hauteur
+        self.largeurEcran=self.root.winfo_screenwidth()
+        self.hauteurEcran=self.root.winfo_screenmmheight()
+        
+        self.cadreterlowExiste=False
 
         self.images={}
         self.cadreactif=None
         self.fullscreen=True
         self.creercadres()
-        self.changecadre(self.cadrebase)
+        self.changecadre(self.cadreterlow)
         
     def changemode(self,cadre):
         if self.modecourant:
@@ -59,42 +47,22 @@ class Vue():
     
         
     def creercadres(self):
-        self.creercadrebase()
+        self.creercadreterlow()
         #self.cadrejeu=Frame(self.root,bg="blue")
         #self.modecourant=None
-        
-    def requetemodule(self,mod):
-        rep=self.serveur.requetemodule(mod)
-        if rep:
-            print(rep[0])
-            cwd=os.getcwd()
-            lieuApp="/gp_"+rep[0]
-            lieu=cwd+lieuApp
-            print(lieu)
-            if not os.path.exists(lieu):
-                os.mkdir(lieu) #plante s'il exist deja
-            reso=rep[1]
-            print(rep[1])
-            for i in rep[2]:
-                if i[0]=="fichier":
-                    nom=reso+i[1]
-                    rep=self.serveur.requetefichier(nom)
-                    fiche=open(lieu+"/"+i[1],"wb")
-                    fiche.write(rep.data)
-                    fiche.close()
-            chaineappli="."+lieuApp+lieuApp+".py"
+    def afficherImage(self):
+        image= Image.open("./terlow.png")
+        image= image.resize((self.largeur, self.hauteur), Image.ANTIALIAS)
 
-            self.pid = Popen([sys.executable, chaineappli,self.monnom,self.monip,self.nodeport],shell=0) 
-        else:
-            print("RIEN") 
-            
+        self.img=ImageTk.PhotoImage(image)
+        self.cadreterlow.create_image(0,0,image=self.img,anchor=NW)
     def creermenu(self):
         self.menubar = Menu(self.root)
 
         self.filemenu = Menu(self.menubar, tearoff=0)
         
         self.filemenu = Menu(self.menubar, tearoff=0)
-        self.filemenu.add_command(label="Nouveau Projet", command=self.salutations)
+        self.filemenu.add_command(label="Nouvelle colonne", command=self.salutations)
         self.filemenu.add_command(label="Ouvrir", command=self.salutations)
         self.filemenu.add_command(label="Enregistrer", command=self.salutations)
         self.filemenu.add_command(label="Enregistrer sous ...", command=self.salutations)
@@ -132,76 +100,19 @@ class Vue():
         #self.frame.pack()
         #self.frame.bind("<Button-3>", self.popup)
         self.root.config(menu=self.menubar) 
-    def fullScreenMode(self): 
-        if(self.fullscreen):
-            self.fullscreen=False
-            self.largeur=self.largeurDefault
-            self.hauteur=self.hauteurDefault
-            self.root.attributes("-fullscreen", False)
-            self.creercadres()
-            self.changecadre(self.cadrebase)
-        else:
-             self.fullscreen=True
-             self.largeur=self.root.winfo_screenwidth()/7
-             self.hauteur=self.root.winfo_screenmmheight()/4.5
-             self.root.attributes("-fullscreen", True)
-             self.creercadres()
-             self.changecadre(self.cadrebase)
-    def destroyCadreBase(self):
-        self.cadrebase.destroy()
-        self.boutonAnalyse.destroy()
-        self.affichagemenu.destroy()
-        self.boutonBudget.destroy()
-        self.boutonCasUsage.destroy()
-        self.boutonCrc.destroy()
-        self.boutonDonnee.destroy()
-        self.boutonMandat.destroy()
-        self.boutonMaquette.destroy()
-        self.boutonProjet1.destroy()
-        self.boutonProjet2.destroy()                        
-        self.boutonProjet3.destroy()
-        self.boutonScrum.destroy()
-        self.boutonTchat.destroy()
-        self.boutonTerlow.destroy()                      
+
+               
               
-    def creercadrebase(self):
-        self.cadrebase=Frame(self.root)
+    def creercadreterlow(self):
+        #permet d'intégrer l'application dans l'application de base
+        self.root.overrideredirect(True) #Enleve la bordure
+        self.root.geometry('%dx%d+%d+%d' % (self.largeurDefault, self.hauteurDefault, (self.largeurEcran/2)-(self.largeurDefault/2),(self.hauteurEcran/2)))
 
-        self.scroll = Scrollbar(self.root)
-        self.mandatTexte = Text(self.root,bg="#09436B",height=int(self.hauteur/4),foreground="white")
-        self.scroll.grid(row=1,column=3,rowspan=4)
-        self.mandatTexte.grid(row=1,column=3,columnspan=6)
-        self.scroll.config(command=self.mandatTexte.yview)
-        self.mandatTexte.config(yscrollcommand=self.scroll.set)
-        self.mandatTexte.insert(INSERT, "MANDAT :")
-        self.mandatTexte.config(state=DISABLED)
-
-        #self.fontTitle = tkFont.Font(family="Helvetica",size=36,weight="bold")
-        self.projetTexte = Entry(self.root,foreground="black",font="-size 34", width=32,background="#526EFF",justify='center')
-        self.projetTexte.grid(row=5,column=2,columnspan=8)
-        self.projetTexte.insert(INSERT, self.projetName)
-        self.projetTexte.config(state=DISABLED)
+        self.cadreterlow=Canvas(self.root,width=self.largeur,height=self.hauteur)
+        self.cadreterlow.grid()
+        self.afficherImage()
         
-        self.listeSprint = Listbox(self.root,width=30, font="-size 16",height=int(self.hauteur/6))
-        self.listeSprint.grid(row=6,column=4,rowspan=4,columnspan=4)
-        for sprint in self.dicodesprint.keys():
-                 self.listeSprint.insert(END,"Sprint " + str(sprint) + "................."+self.dicodesprint[sprint])
-        
-        
-        self.listeTexte = Entry(self.root,foreground="black", font="-size 16",width=80,background="#526EFF",justify='center')
-        self.listeTexte.grid(row=10,column=2,columnspan=8)
-        self.listeTexte.insert(INSERT, "Membre \t\t\t\t Travail sur")
-        self.listeTexte.config(state=DISABLED)
-        
-        self.listeMembre = Listbox(self.root,width=30, font="-size 16",height=int(self.hauteur/5))
-        self.listeMembre.grid(row=11,column=3,rowspan=5,columnspan=4)
-        for membre in self.utilisateursEtRole.keys():
-                 self.listeMembre.insert(END,membre )
-        self.listeOccupation = Listbox(self.root,width=30, font="-size 16",height=int(self.hauteur/5))
-        self.listeOccupation.grid(row=11,column=6,rowspan=5,columnspan=4)
-        for membre in self.utilisateursEtRole.keys():
-                 self.listeOccupation.insert(END,self.utilisateursEtRole[membre])
-        self.cadrebaseExiste=True
+        self.cadreterlowExiste=True
     def accesScrum(self):
         ad="http://"+ipserveur+":"+self.nodeport
         self.serveur=ServerProxy(ad)

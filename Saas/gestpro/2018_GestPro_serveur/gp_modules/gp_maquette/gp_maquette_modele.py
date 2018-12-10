40d428a023b384bd7969aa1638e12d153f8d4661
Doutre-Guay Ludovic <1349519@decinfo.cvm>
2018-12-10 10:36:16 -0500

    ff

--- Saas/gestpro/2018_GestPro_serveur/gp_modules/gp_maquette/gp_maquette_modele.py
+++ Saas/gestpro/2018_GestPro_serveur/gp_modules/gp_maquette/gp_maquette_modele.py
@@ -17,8 +17,7 @@ class Modele():
     def sauvegarde(self,listeObjet):
         for objet in listeObjet :
            self.BD.requeteInsertionPerso("INSERT INTO Objet_Maquette(Type, PosX,PosY,X,Y,Bordure,Interieur,texte, Font) VALUES('" + str(objet[0]) +"','"+ str(objet[1]) +"','"+ str(objet[2]) +"','"+ str(objet[3]) +"','"+ str(objet[4]) +"','"+ str(objet[5]) +"','"+ str(objet[6]) +"','"+ str(objet[7]) +"','"+ str(objet[8]) + "');")
-
-
+            
                     
     def selectAffichage(self):
         try :
